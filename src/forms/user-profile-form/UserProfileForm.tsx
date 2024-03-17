2dcc100c8d67b335a8ef299530def06069b2385c
ShaanKapoor10 <shaan2003kapoor@gmail.com>
2024-03-18 04:28:51 +0530

    add to cart ui and menu item implemented

--- src/forms/user-profile-form/UserProfileForm.tsx
+++ src/forms/user-profile-form/UserProfileForm.tsx
@@ -1,52 +1,68 @@
-import LoadingButton from "@/components/LoadingButton";
-import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { User } from "@/types";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import LoadingButton from "@/components/LoadingButton";
+import { Button } from "@/components/ui/button";
+import { User } from "@/types";
+import { useEffect } from "react";
 
 const formSchema = z.object({
-    email: z.string().optional(),
-    name: z.string().min(1,"name is required"),
-    addressLine1: z.string().min(1,"Address Line is required"),
-    city: z.string().min(1,"City is required"),
-    country: z.string().min(1,"Country"),
+  email: z.string().optional(),
+  name: z.string().min(1, "name is required"),
+  addressLine1: z.string().min(1, "Address Line 1 is required"),
+  city: z.string().min(1, "City is required"),
+  country: z.string().min(1, "Country is required"),
 });
 
-type UserFormData = z.infer<typeof formSchema>;
+export type UserFormData = z.infer<typeof formSchema>;
 
 type Props = {
-    currentUser : User
-    onSave: (userProfileData: UserFormData)=> void;
-    isLoading : boolean;
-}
+  currentUser: User;
+  onSave: (userProfileData: UserFormData) => void;
+  isLoading: boolean;
+  title?: string;
+  buttonText?: string;
+};
 
-const UserProfileForm = ({ onSave , isLoading ,currentUser } : Props) => {
-    const form =useForm<UserFormData>({
-        resolver: zodResolver(formSchema),
-        defaultValues :  currentUser, 
-    });
-    
-    useEffect(() => {
-      form.reset(currentUser);
-    }, [currentUser, form]);
-  
+const UserProfileForm = ({
+  onSave,
+  isLoading,
+  currentUser,
+  title = "User Profile",
+  buttonText = "Submit",
+}: Props) => {
+  const form = useForm<UserFormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: currentUser,
+  });
 
+  useEffect(() => {
+    form.reset(currentUser);
+  }, [currentUser, form]);
 
-    return(
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSave)} className="space-y-4 bg-gray-50 rounded-lg md:p-10">
-                <div>
-                    <h2 className="text-2xl font-bold">User Profile Form</h2>
-                    <FormDescription>
-                        View and change your profile information here
-                    </FormDescription>
-                </div>
-                <FormField
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSave)}
+        className="space-y-4 bg-gray-50 rounded-lg md:p-10"
+      >
+        <div>
+          <h2 className="text-2xl font-bold">{title}</h2>
+          <FormDescription>
+            View and change your profile information here
+          </FormDescription>
+        </div>
+        <FormField
           control={form.control}
           name="email"
           render={({ field }) => (
@@ -118,12 +134,12 @@ const UserProfileForm = ({ onSave , isLoading ,currentUser } : Props) => {
           <LoadingButton />
         ) : (
           <Button type="submit" className="bg-orange-500">
-            Submit
+            {buttonText}
           </Button>
         )}
-            </form>
-        </Form>
-    )
+      </form>
+    </Form>
+  );
 };
 
 export default UserProfileForm;
