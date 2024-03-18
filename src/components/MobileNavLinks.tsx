55d2c9bec587e934375401cb608dd84af3526735
ShaanKapoor10 <shaan2003kapoor@gmail.com>
2024-03-18 06:24:09 +0530

    Added Manage Restraurant Hamburger menu

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -12,6 +12,11 @@ const MobileNavLinks = () => {
             >
                 User Profile
             </Link>
+            <Link to="/manage-restaurant"
+                 className="flex bg-white items-center font-bold hover:text-orange-500"
+            >
+                Manage Restraurant
+            </Link>
             <Button onClick={() => logout()} className="flex items-center px-3 font-bold hover:bg-gray-500">
                 Log Out
             </Button>
