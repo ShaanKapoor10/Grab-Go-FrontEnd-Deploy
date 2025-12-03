823fd55bf640dc85892bb7cdad0a23117811c4ea
ANKIT RAJ <111572476+raaj6395@users.noreply.github.com>
2025-12-04 02:03:35 +0530

    fix the hero image size

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ import SearchBar, { SearchForm } from "@/components/SearchBar";
 import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const handleSearchSubmit = (searchFormValues: SearchForm) => {
     navigate({
@@ -12,9 +12,9 @@ const HomePage = () => {
     });
   };
 
-    return(
-        <div className="flex flex-col gap-12">
-            <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+  return (
+    <div className="flex flex-col gap-12">
+      <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
         <h1 className="text-5xl font-bold tracking-tight text-orange-600">
           Tuck into a takeway today
         </h1>
@@ -24,20 +24,20 @@ const HomePage = () => {
           onSubmit={handleSearchSubmit}
         />
       </div>
-            <div className="grid md:grid-cols-2 gap-5">
-                <img src={landingImage}/>
-                <div className="flex flex-col items-center justify-center gap-4 text-center">
-                    <span className="font-bold text-3xl tracking-tighter">
-                        Order takeaway even faster!
-                    </span>
-                    <span>
-                        Visit the Grab&Go.com for faster ordering and personalised recommendations 
-                    </span>
-                    <img src={appDownloadImage}/>
-                </div>
-            </div>
+      <div className="grid md:grid-cols-2 gap-5">
+        <img src={landingImage} />
+        <div className="flex flex-col items-center justify-center gap-4 text-center">
+          <span className="font-bold text-3xl tracking-tighter">
+            Order takeaway even faster!
+          </span>
+          <span>
+            Visit the Grab&Go.com for faster ordering and personalised recommendations
+          </span>
+          <img src={appDownloadImage} />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
